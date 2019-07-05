c953b12e2625d4400704a52b110f00108427318c
Rico Kahler <ricokahler@me.com>
2019-07-05 13:39:53 -0400

    move lodash and superagent back to deps

--- webpack.config.js
+++ webpack.config.js
@@ -36,5 +36,5 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.json', '.jsx', '.css', '.ts'],
   },
-  externals: ['react', 'react-dom', 'react-redux', 'lodash', 'superagent'],
+  externals: ['react', 'react-dom', 'react-redux'],
 };
