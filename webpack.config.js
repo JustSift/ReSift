2a17f83878ec37c7ed1112950072cf828f06752c
Rico Kahler <ricokahler@me.com>
2019-07-04 20:01:14 -0400

    wip build system

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname),
+    libraryTarget: 'umd',
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        include: [path.resolve(__dirname, './src')],
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  ie: '11',
+                },
+              },
+            ],
+            '@babel/preset-react',
+            '@babel/preset-typescript',
+          ],
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.css', '.ts'],
+  },
+};
