00400fbd768ed1e73f7ddd4cdb962b82ab57edf7
Rico Kahler <ricokahler@me.com>
2019-08-19 16:54:54 -0400

    revert

--- src/clearFetch/clearFetch.d.ts
+++ src/clearFetch/clearFetch.d.ts
@@ -1,6 +1,8 @@
-import { FetchActionCreator } from '../defineFetch';
+import { FetchActionCreator, FetchActionMeta } from '../defineFetch';
 
-export type ClearFetchAction = ReturnType<typeof clearFetch>;
-
+export interface ClearFetchAction {
+  type: string;
+  meta: FetchActionMeta;
+}
 export function isClearAction(action: any): action is ClearFetchAction;
-export default function clearFetch(fetch: FetchActionCreator): any;
+export default function clearFetch(fetch: FetchActionCreator): ClearFetchAction;
