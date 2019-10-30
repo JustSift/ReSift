a7e3c15524176e4f82962fe0a7b92316722f4c46
Rico Kahler <ricokahler@me.com>
2019-10-30 10:47:38 -0400

    allow useData and useStatus to take in `null`

--- src/useData/useData.d.ts
+++ src/useData/useData.d.ts
@@ -5,9 +5,11 @@ import { FetchInstance } from '../defineFetch';
  *
  * Grabs the data out of the `FetchInstance`. This function may return `null`
  * if there is no data available yet.
+ *
+ * If you pass in `null` (or any other falsy value), you'll get `null` back out
  */
 declare function useData<FetchArgs extends any[], Data = any>(
-  fetch: FetchInstance<FetchArgs, Data>,
+  fetch: FetchInstance<FetchArgs, Data> | null,
 ): Data | null;
 
 export default useData;
--- src/useStatus/useStatus.d.ts
+++ src/useStatus/useStatus.d.ts
@@ -4,7 +4,9 @@ import { FetchInstance } from '../defineFetch';
  * @docs `useStatus`
  *
  * Returns the status of a fetch given a fetch instance
+ *
+ * If you pass in `null` (or any other falsy value), you'll get `UNKNOWN` back out
  */
-declare function useStatus(fetch: FetchInstance): number;
+declare function useStatus(fetch: FetchInstance | null): number;
 
 export default useStatus;
